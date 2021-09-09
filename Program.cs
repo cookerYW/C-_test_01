f33a89282baee7b12798528eada09e21935dbfd8
cookerYW <87749633+cookerYW@users.noreply.github.com>
2021-09-09 23:28:23 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_01
+{
+    class Car
+    {
+        private string handle = "Amodel";
+
+        public void run()
+        {
+            Console.WriteLine("달리기");
+        }
+    }
+    
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("글자찍기");
+    //        //Ctrl + Shift + B --> 빌드
+    //        //저장먼저 하고나서 빌드해야함.
+    //        //CTRL + F5 --> 실행
+    //    }
+    //}
+}
